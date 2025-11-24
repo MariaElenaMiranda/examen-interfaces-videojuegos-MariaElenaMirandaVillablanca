19f8abd69a37015661fbb191758354a2dc79b3b9
Maria Elena Miranda <ifp.mara@gmail.com>
2025-11-24 17:49:19 +0100

    Implementación de getJuegosPorPlataformaService

---
+++ src/services/getJuegosPorPlataformaService.js
@@ -0,0 +1,17 @@
+import { juegos } from '../../public/data/info.js'
+
+export function getJuegosPorPlataforma(categoria) {
+  console.log('----------------------------------------------');
+  console.log('Llamando a función getJuegoPorPlataforma();');
+  console.log('----------------------------------------------');
+
+  const promesa = new Promise((resolve, reject) => {
+    setTimeout(() => {
+      const listadoJuegos = juegos.filter(juego => juego.categoria === categoria);
+      if(listadoJuegos) {
+        resolve(listadoJuegos);
+      }
+    },800);
+  });
+  return promesa;
+}
