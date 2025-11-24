ee13d4589b6d2445b058a1a7c648fd948a06bdb3
Maria Elena Miranda <ifp.mara@gmail.com>
2025-11-24 17:30:56 +0100

    Implementación de getJuegoService

---
+++ src/services/getJuegoService.js
@@ -0,0 +1,20 @@
+import { juegos } from '../../public/data/info.js'
+
+export function getJuego(id) {
+  console.log('----------------------------------------------');
+  console.log('Llamando a función getJuego;');
+  console.log('----------------------------------------------');
+
+  const promesa = new Promise((resolve, reject) => {
+    setTimeout(() => {
+      const juego = juegos.find(juego => juego.id === Number(id))?.titulo;
+      if(juego) {
+        resolve(juego);
+      }else {
+        reject(`El juego con ID ${id} no existe.`)
+      }
+    },800);
+  });
+  return promesa;
+}
+
